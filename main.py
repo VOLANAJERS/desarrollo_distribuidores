0313a47d57cef976edabf56db00ab4fc567bd52b
jers22 <152739931+jers22@users.noreply.github.com>
2024-02-02 07:07:56 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+with st.sidebar:
+    
+    page = st.selectbox(
+    'Selecciona la página',
+    ('Desarrollo de distribuidores')
+
+    
+if page == 'Desarrollo de distribuidores':
+    
+    st.title("Desarrollo de distribuidores")
+    
